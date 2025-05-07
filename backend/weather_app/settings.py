d3f4a17dff2519cf380e2e45ba591b5c56e3b6af
Dodi <dodiraditya@gmail.com>
2025-05-07 08:41:01 +0200

    New docker compose setup

--- backend/weather_app/settings.py
+++ backend/weather_app/settings.py
@@ -79,9 +79,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.{}".format(
             os.getenv("DATABASE_ENGINE", "sqlite3")
         ),
-        "NAME": os.getenv("DATABASE_NAME", "polls"),
-        "USER": os.getenv("DATABASE_USERNAME", "myprojectuser"),
-        "PASSWORD": os.getenv("DATABASE_PASSWORD", "password"),
+        "NAME": os.getenv("DATABASE_NAME", "mydatabase"),
+        "USER": os.getenv("DATABASE_USERNAME", "myuser"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "mypassword"),
         "HOST": os.getenv("DATABASE_HOST", "127.0.0.1"),
         "PORT": os.getenv("DATABASE_PORT", 5432),
     }
