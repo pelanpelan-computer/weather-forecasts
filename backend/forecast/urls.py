2754ce558d3f51fb15ea7c03980a6df906c8059e
Dodi <dodiraditya@gmail.com>
2025-06-09 14:21:28 +0200

    Get real data

--- backend/forecast/urls.py
+++ backend/forecast/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("search", views.search),
+    path("<str:slug>/<str:lat>/<str:lon>/", views.city_forecast),
+    # path("", views.index, name="index"),
 ]
--- backend/forecast/views.py
+++ backend/forecast/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from meteofrance_api import MeteoFranceClient
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the forecast index.")
+def search(request):
+    q = request.GET.get("q", None)
+    client = MeteoFranceClient()
+    places = client.search_places(q)
+    places_list = [place.__dict__["raw_data"] for place in places]
+    return JsonResponse(places_list, safe=False)
+
+
+def city_forecast(request, slug, lat, lon):
+    client = MeteoFranceClient()
+    forecast = client.get_forecast(lat, lon).daily_forecast
+    return JsonResponse(forecast, safe=False)
